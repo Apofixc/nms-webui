b83a4b990f2f7f0d03177c225507d8692368027a
Apofixc <mihail.hodorovich@mail.ru>
2026-08-08 11:33:11 +0300

    refactor: remove Tuya module and all associated frontend and backend files

--- backend/modules/tuya/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Точка входа модуля управления устройствами Tuya."""
-from backend.core.plugin.context import ModuleContext
-from backend.modules.tuya.module import TuyaModule
-
-
-def create_module(ctx: ModuleContext) -> TuyaModule:
-    """Фабричная функция создания экземпляра модуля Tuya."""
-    return TuyaModule(ctx)
